c2c8138405584e9444eb04c584bc6c9b0202e6ac
Gary Kim <gary@ydgkim.com>
2019-03-23 00:06:39 +0800

    Better help and error messages for build script

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -e
+
 if [[ "$1" == "--help" ]] || [[ "$1" == "help" ]] || [[ "$1" == "-h" ]]; then
-    echo "Avaliable Options: [chromium],[firefox],[chromiumtest],[chromiumtestremove]"
-fi
-if [[ "$1" == "chromium" ]]; then
+ echo -e "Build script for SAS Powerschool Enhancement Suite\n\n    Usage: ./build.sh OPTION\n\nOptions:\n    firefox            Build packaged version for Firefox\n    chromium           Build packaged version for Chromium\n    chromiumtest       Create folder to load unpackaged into Chromium\n    chromiumtestremove Remove folder made by './build.sh chromiumtest' command\n\n SAS Powerschool Enhancement Suite is licensed under GPL-3.0."
+
+elif [[ "$1" == "chromium" ]]; then
     mkdir tmp
     cp ./src/{chromium,css,icons,js,lib,ui,'manifest - chromium.json'} ./tmp -r
     mv "./tmp/manifest - chromium.json" "./tmp/manifest.json"
@@ -11,25 +13,26 @@ if [[ "$1" == "chromium" ]]; then
     zip -r9 ../chromium_build.zip ./
     cd ..
     rm -r ./tmp
-    echo Chromium build complete. Saved as chromium_build.zip
-fi
-if [[ "$1" == "firefox" ]]; then
+    echo -e "Chromium build complete. Saved as chromium_build.zip"
+
+elif [[ "$1" == "firefox" ]]; then
     mkdir tmp
     cp ./src/{css,icons,js,lib,ui,manifest.json} ./tmp -r
     cd tmp
     zip -r9 ../firefox_build.zip ./
     cd ..
     rm -r ./tmp
-    echo Firefox build complete. Saved as firefox_build.zip
-fi
-if [[ "$1" == "chromiumtest" ]]; then
+    echo -e "Firefox build complete. Saved as firefox_build.zip"
+
+elif [[ "$1" == "chromiumtest" ]]; then
     mkdir chromiumtest
     cp ./src/{chromium,css,icons,js,lib,ui,'manifest - chromium.json'} ./chromiumtest -r
     mv "./chromiumtest/manifest - chromium.json" "./chromiumtest/manifest.json"
-    echo Chromium test folder made
-fi
-if [[ "$1" == "chromiumtestremove" ]]; then
-    rm -r ./chromiumtest
-    echo Chromium test folder removed
-fi
+    echo -e "Chromium test folder made"
 
+elif [[ "$1" == "chromiumtestremove" ]]; then
+    rm -r ./chromiumtest
+    echo -e "Chromium test folder removed"
+else
+    echo -e "Usage: ./build.sh OPTION\nTry './build.sh --help' for more information."
+fi

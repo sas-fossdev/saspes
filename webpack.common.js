ccd07dbc7e58413bc6771fc86349ce527c24e83c
Gary Kim <gary@garykim.dev>
2019-07-08 11:52:45 +0800

    td: add configuration to use vue
    Signed-off-by: Gary Kim <gary@garykim.dev>

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = {
     entry: {
@@ -15,6 +16,14 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.css$/,
+                use: ['vue-style-loader', 'css-loader']
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
                 test: /\.js$/,
                 loader: 'babel-loader'
             }
@@ -23,7 +32,8 @@ module.exports = {
     plugins: [
         new CopyPlugin([
             { from: "src", to: '', ignore: ['*.js', 'manifest.json', 'manifest - chromium.json', '.eslintrc.json']}
-        ])
+        ]),
+        new VueLoaderPlugin()
     ],
     resolve: {
         extensions: ['.js']

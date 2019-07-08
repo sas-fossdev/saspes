69eef209ff7438e2469a548c4a9375a7a0fa2331
Gary Kim <gary@garykim.dev>
2019-07-09 00:26:01 +0800

    webpack: configure for less
    Signed-off-by: Gary Kim <gary@garykim.dev>

--- webpack.common.js
+++ webpack.common.js
@@ -16,6 +16,10 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.less$/,
+                use: ['vue-style-loader', 'css-loader', 'less-loader']
+            },
+            {
                 test: /\.css$/,
                 use: ['vue-style-loader', 'css-loader']
             },

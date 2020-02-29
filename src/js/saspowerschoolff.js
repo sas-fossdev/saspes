3884f8d166e4d3d9193a40271b097b0e829cd263
Suhas Hariharan <hariharan774531@sas.edu.sg>
2020-03-01 00:02:52 +0800

    Shows first semester GPA during second semester.
    Makes a get request to the grade history page and uses that to show first semester GPA.
    
    Signed-off-by: Suhas Hariharan hariharan774531@sas.edu.sg

--- src/js/saspowerschoolff.js
+++ src/js/saspowerschoolff.js
@@ -76,6 +76,7 @@ function main_page () {
     const student_name = document.querySelector('#userName').querySelector('span').innerText;
     let second_semester = false;
     const courses = [];
+    const courses_first_semester = [];
     const $grade_rows = $('#quickLookup table.grid').find('tr');
     let s1col = 0;
     let s2col = 0;
@@ -96,6 +97,39 @@ function main_page () {
             }
             curr += parseInt(e.getAttribute('colspan')) || 1;
         });
+    }
+    if (second_semester) {
+        let response=$.ajax({
+
+            url: "https://powerschool.sas.edu.sg/guardian/termgrades.html",
+
+        }).done(function (data) {
+            let el = document.createElement("html");
+            let element_list = [];
+            el.innerHTML=data;
+            element_list=el.getElementsByClassName("box-round")[0].getElementsByTagName("table")[0];
+            element_list=element_list.getElementsByTagName("tbody")[0].getElementsByTagName("tr")
+            if (element_list.length>2) {
+                for (let i = 2; i < element_list.length; i++) {
+                    let $prev_course=element_list[i];
+        
+                    
+                    courses_first_semester.push({
+                        name: $prev_course.getElementsByTagName("td")[0].textContent.trim(),
+                        grade: $prev_course.getElementsByTagName("td")[1].textContent.trim(),
+                        link: $prev_course.getElementsByTagName("td")[2].innerHTML.trim().split('href="')[1].split("&amp")[0],
+                        fp: -1,
+                    });
+        
+                 
+        
+        
+                }
+                $("table[border='0'][cellpadding='3'][cellspacing='1'][width='100%']").prepend(`<tr><td align="center">Last Semester GPA (S1): ${calculate_gpa(courses_first_semester)}</td></tr>`);
+
+            } 
+        })
+        
     }
     for (let i = 0; i < $grade_rows.length; i++) {
         let $course;

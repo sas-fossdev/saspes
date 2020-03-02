df0a6d435fe3b36cdabfb17367c1320a3b0139f0
Suhas Hariharan <hariharan774531@sas.edu.sg>
2020-03-02 19:03:54 +0800

    Shows first semester GPA during second semester
    Signed-off-by: Suhas Hariharan <hariharan774531@sas.edu.sg>

--- src/js/saspowerschoolff.js
+++ src/js/saspowerschoolff.js
@@ -98,32 +98,6 @@ function main_page () {
             curr += parseInt(e.getAttribute('colspan')) || 1;
         });
     }
-    if (second_semester) {
-        fetch("https://powerschool.sas.edu.sg/guardian/termgrades.html")
-            .then((response) => {
-                return response.text();
-            })
-            .then((data) => {
-                const el = document.createElement("html");
-                let element_list = [];
-                el.innerHTML = data;
-                element_list = el.getElementsByClassName("box-round")[0].getElementsByTagName("table")[0];
-                element_list = element_list.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
-                if (element_list.length > 2) {
-                    for (let i = 2; i < element_list.length; i++) {
-                        const $prev_course = element_list[i];
-                        courses_first_semester.push({
-                            name: $prev_course.getElementsByTagName("td")[0].textContent.trim(),
-                            grade: $prev_course.getElementsByTagName("td")[1].textContent.trim(),
-                            link: $prev_course.getElementsByTagName("td")[2].getElementsByTagName("a")[0].href,
-                            fp: -1,
-
-                        });
-                    }
-                    $("table[border='0'][cellpadding='3'][cellspacing='1'][width='100%']").prepend(`<tr><td align="center">Last Semester GPA (S1): ${calculate_gpa(courses_first_semester)}</td></tr>`);
-                }
-            });
-    }
     for (let i = 0; i < $grade_rows.length; i++) {
         let $course;
         if (second_semester) {
@@ -152,7 +126,32 @@ function main_page () {
         }
     }
     $("table[border='0'][cellpadding='3'][cellspacing='1'][width='100%']").prepend(`<tr><td align="center">Current Semester GPA (${second_semester ? 'S2' : 'S1'}): ${calculate_gpa(courses)}</td></tr>`);
+    if (second_semester) {
+        fetch("https://powerschool.sas.edu.sg/guardian/termgrades.html")
+            .then((response) => {
+                return response.text();
+            })
+            .then((data) => {
+                const el = document.createElement("html");
+                let element_list = [];
+                el.innerHTML = data;
+                element_list = el.getElementsByClassName("box-round")[0].getElementsByTagName("table")[0];
+                element_list = element_list.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
+                if (element_list.length > 2) {
+                    for (let i = 2; i < element_list.length; i++) {
+                        const $prev_course = element_list[i];
+                        courses_first_semester.push({
+                            name: $prev_course.getElementsByTagName("td")[0].textContent.trim(),
+                            grade: $prev_course.getElementsByTagName("td")[1].textContent.trim(),
+                            link: $prev_course.getElementsByTagName("td")[2].getElementsByTagName("a")[0].href,
+                            fp: -1,
 
+                        });
+                    }
+                    $("table[border='0'][cellpadding='3'][cellspacing='1'][width='100%']").prepend(`<tr><td align="center">Last Semester GPA (S1): ${calculate_gpa(courses_first_semester)}</td></tr>`);
+                }
+            });
+    }
     // Hypo Grade Calculator
     const HypoGradesDiv = document.createElement('div');
     HypoGradesDiv.classList.add("hypo-grade-div-fixed");

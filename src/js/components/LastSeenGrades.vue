224c33380e72b790de9ed9e85570193cb723a353
Suhas Hariharan <hariharan774531@sas.edu.sg>
2020-03-23 19:53:18 +0800

    Added function to get local grades and save local grades to chrome storage API
    Signed-off-by: Suhas Hariharan <hariharan774531@sas.edu.sg>

---
+++ src/js/components/LastSeenGrades.vue
@@ -0,0 +1,32 @@
+<!--
+ - @copyright Copyright (c) 2018-2019 Suhas Hariharan <mail@suhas.net>
+ -
+ - @author Suhas Hariharan <mail@suhas.net>
+ -
+ - @license GNU AGPL version 3 only
+ -
+ - SAS Powerschool Enhancement Suite - A browser extension to improve the experience of SAS Powerschool.
+ -
+ - This program is free software: you can redistribute it and/or modify
+ - it under the terms of the GNU Affero General Public License as
+ - published by the Free Software Foundation, version 3.
+ -
+ - This program is distributed in the hope that it will be useful,
+ - but WITHOUT ANY WARRANTY; without even the implied warranty of
+ - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ - GNU Affero General Public License for more details.
+ -
+ - You should have received a copy of the GNU Affero General Public License
+ - along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ -->
+<template>
+    <div>
+        Last Seen Grades <div/>
+    </div>
+</template>
+<script>
+
+</script>
+<style lang="less" scoped>
+
+</style>

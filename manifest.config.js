78bbeafa5c867d397c8921a93cba6d35190c8f0a
Anvay Mathur <suggestions@megagames.me>
2024-01-04 14:20:20 +0800

    fix building
    Signed-off-by: Anvay Mathur <suggestions@megagames.me>

--- manifest.config.js
+++ manifest.config.js
@@ -1,15 +1,17 @@
 import { defineManifest } from '@crxjs/vite-plugin'
 import packageJson from './package.json'
+import 'dotenv/config'
 const { version } = packageJson
 
 
 
+export const realVersion = `${new Date().getUTCFullYear()}.${new Date().getUTCMonth() + 1}${String(new Date().getUTCDate()).padStart(2, "0")}.${new Date().getUTCMinutes()}${String(new Date().getUTCHours()).padStart(2, "0")}.${new Date().getUTCSeconds()}${String(new Date().getUTCMilliseconds()).padStart(3, "0")}`;
 export default defineManifest(async (env) => ({
   manifest_version: 3,
   // In UTC time: YYYY.MMDD.HHMM.SSmmm
-  version: `${new Date().getUTCFullYear()}.${new Date().getUTCMonth() + 1}${String(new Date().getUTCDate()).padStart(2, "0")}.${new Date().getUTCMinutes()}${String(new Date().getUTCHours()).padStart(2, "0")}.${new Date().getUTCSeconds()}${String(new Date().getUTCMilliseconds()).padStart(3, "0")}`,
+  version: realVersion,
   // semver is OK in "version_name"
-  version_name: version,
+  version_name: version + (process.argv[4] === "production" ? "" : ` Dev Build ${realVersion}`),
   "name": "SAS Powerschool Enhancement Suite",
   "description": "Provides various enhancements for SAS Powerschool",
   "action": {
